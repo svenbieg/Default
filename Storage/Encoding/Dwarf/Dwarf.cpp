87e48917f41ed7264d37bc6d7372f45bb7c13458
Sven Bieg <svenbieg@web.de>
2024-11-06 19:16:07 +0100

    Added Dwarf encoding

---
+++ Storage/Encoding/Dwarf/Dwarf.cpp
@@ -0,0 +1,158 @@
+//===========
+// Dwarf.cpp
+//===========
+
+// DWARF was orginally developed by Bell Labs,
+// licensed under the GNU Free Documentation Standard
+
+#include "pch.h"
+
+
+//=======
+// Using
+//=======
+
+#include "Dwarf.h"
+
+
+//===========
+// Namespace
+//===========
+
+namespace Storage {
+	namespace Encoding {
+
+
+//========
+// Common
+//========
+
+SIZE_T Dwarf::Read(BYTE const*& dwarf)noexcept
+{
+BYTE encoding=*dwarf++;
+return ReadEncoded(dwarf, encoding);
+}
+
+SIZE_T Dwarf::ReadEncoded(BYTE const*& dwarf, BYTE encoding, SIZE_T relative)noexcept
+{
+SIZE_T ptr_rel=(SIZE_T)dwarf;
+SIZE_T value=0;
+UINT type=encoding&0xF;
+if(type==0xF)
+	return 0;
+switch(type)
+	{
+	case DW_POINTER_ABS:
+		value=ReadValue<SIZE_T>(dwarf);
+		break;
+	case DW_UNSIGNED:
+		value=ReadUnsigned(dwarf);
+		break;
+	case DW_WORD:
+		value=ReadValue<WORD>(dwarf);
+		break;
+	case DW_DWORD:
+		value=ReadValue<DWORD>(dwarf);
+		break;
+	case DW_QWORD:
+		value=ReadValue<QWORD>(dwarf);
+		break;
+	case DW_SIGNED:
+		value=ReadSigned(dwarf);
+		break;
+	case DW_SHORT:
+		value=ReadValue<SHORT>(dwarf);
+		break;
+	case DW_INT:
+		value=ReadValue<INT>(dwarf);
+		break;
+	case DW_LONG:
+		value=ReadValue<INT64>(dwarf);
+		break;
+	default:
+		Abort();
+	}
+UINT mode=encoding&0x70;
+switch(mode)
+	{
+	case DW_POINTER_ABS:
+		break;
+	case DW_POINTER_REL:
+		{
+		value+=ptr_rel;
+		break;
+		}
+	case DW_DATA_REL:
+		{
+		value+=relative;
+		break;
+		}
+	default:
+		Abort();
+	}
+if(GetFlag(encoding, DW_INDIRECT))
+	value=*(SIZE_T*)value;
+return value;
+}
+
+SIZE_T Dwarf::ReadSigned(BYTE const*& dwarf)noexcept
+{
+SIZE_T value=0;
+UINT shift=0;
+BYTE byte;
+do
+	{
+	byte=*dwarf++;
+	value|=((SIZE_T)byte&0x7F)<<shift;
+	shift+=7;
+	}
+while(byte&0x80);
+if(byte&0x40)
+	value|=(-1ULL)<<shift;
+return value;
+}
+
+SIZE_T Dwarf::ReadUnsigned(BYTE const*& dwarf)noexcept
+{
+SIZE_T value=0;
+UINT shift=0;
+BYTE byte;
+do
+	{
+	byte=*dwarf++;
+	value|=((SIZE_T)byte&0x7F)<<shift;
+	shift+=7;
+	}
+while(byte&0x80);
+return value;
+}
+
+UINT Dwarf::GetSize(BYTE encoding)noexcept
+{
+UINT size=0;
+switch(encoding&0x0F)
+	{
+	case DW_OMIT:
+	case DW_UNSIGNED:
+	case DW_SIGNED:
+		break;
+	case DW_WORD:
+	case DW_SHORT:
+		size=2;
+		break;
+	case DW_DWORD:
+	case DW_INT:
+		size=4;
+		break;
+	case DW_POINTER_ABS:
+	case DW_QWORD:
+	case DW_LONG:
+		size=8;
+		break;
+	default:
+		Abort();
+	}
+return size;
+}
+
+}}
---
+++ Storage/Encoding/Dwarf/Dwarf.h
@@ -0,0 +1,65 @@
+//=========
+// Dwarf.h
+//=========
+
+// DWARF was orginally developed by Bell Labs,
+// licensed under the GNU Free Documentation Standard
+
+#pragma once
+
+
+//===========
+// Namespace
+//===========
+
+namespace Storage {
+	namespace Encoding {
+
+
+//==========
+// Encoding
+//==========
+
+typedef enum
+{
+DW_POINTER_ABS=0,
+DW_UNSIGNED=1,
+DW_WORD=2,
+DW_DWORD=3,
+DW_QWORD=4,
+DW_SIGNED=9,
+DW_SHORT=10,
+DW_INT=11,
+DW_LONG=12,
+DW_POINTER_REL=0x10,
+DW_TEXT_REL=0x20,
+DW_DATA_REL=0x30,
+DW_FUNC_REL=0x40,
+DW_ALIGNED=0x50,
+DW_INDIRECT=0x80,
+DW_OMIT=0xFF
+}DW_ENCODING;
+
+
+//=======
+// Dwarf
+//=======
+
+class Dwarf
+{
+public:
+	// Common
+	static SIZE_T Read(BYTE const*& Dwarf)noexcept;
+	static SIZE_T ReadEncoded(BYTE const*& Dwarf, BYTE Encoding, SIZE_T Relative=0)noexcept;
+	static SIZE_T ReadSigned(BYTE const*& Dwarf)noexcept;
+	static SIZE_T ReadUnsigned(BYTE const*& Dwarf)noexcept;
+	template <typename _value_t> static inline _value_t ReadValue(BYTE const*& Dwarf)noexcept
+		{
+		_value_t value=*(_value_t const*)Dwarf;
+		Dwarf+=sizeof(_value_t);
+		return value;
+		}
+	static UINT GetSize(BYTE Encoding)noexcept;
+};
+
+}}
