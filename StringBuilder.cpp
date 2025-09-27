f8fc9351cba6eee9db1f78397a29d4ee211e1e73
Sven Bieg <svenbieg@outlook.de>
2025-09-28 01:28:02 +0200

    StringBuilder

--- StringBuilder.cpp
+++ StringBuilder.cpp
@@ -16,27 +16,16 @@
 // Con-/Destructors
 //==================
 
-StringBuilder::StringBuilder():
+StringBuilder::StringBuilder(UINT len):
 m_AppendAnsi(&StringBuilder::BufferAppendAnsi),
 m_AppendUnicode(&StringBuilder::BufferAppendUnicode),
 m_Buffer(64*sizeof(TCHAR)),
-m_Initialize(&StringBuilder::BufferInitialize),
 m_Position(0),
 m_Size(0),
 m_ToString(&StringBuilder::BufferToString)
-{}
-
-StringBuilder::StringBuilder(UINT len):
-m_AppendAnsi(&StringBuilder::StringAppendAnsi),
-m_AppendUnicode(&StringBuilder::StringAppendUnicode),
-m_Buffer(64*sizeof(TCHAR)),
-m_Initialize(&StringBuilder::StringInitialize),
-m_Position(0),
-m_Size(len+1),
-m_ToString(&StringBuilder::StringToString)
 {
-if(len==0)
-	throw InvalidArgumentException();
+if(len)
+	Initialize(len);
 }
 
 
@@ -46,19 +35,16 @@ if(len==0)
 
 UINT StringBuilder::Append(CHAR c)
 {
-(this->*m_Initialize)();
 return (this->*m_AppendAnsi)(c);
 }
 
 UINT StringBuilder::Append(WCHAR c)
 {
-(this->*m_Initialize)();
 return (this->*m_AppendUnicode)(c);
 }
 
 UINT StringBuilder::Append(LPCSTR String)
 {
-(this->*m_Initialize)();
 UINT pos=0;
 for(; String[pos]; pos++)
 	(this->*m_AppendAnsi)(String[pos]);
@@ -67,7 +53,6 @@ return pos;
 
 UINT StringBuilder::Append(LPCWSTR String)
 {
-(this->*m_Initialize)();
 UINT pos=0;
 for(; String[pos]; pos++)
 	(this->*m_AppendUnicode)(String[pos]);
@@ -76,7 +61,6 @@ return pos;
 
 UINT StringBuilder::Append(UINT Length, LPCSTR String)
 {
-(this->*m_Initialize)();
 UINT pos=0;
 for(; String[pos]; pos++)
 	{
@@ -89,7 +73,6 @@ return pos;
 
 UINT StringBuilder::Append(UINT Length, LPCWSTR String)
 {
-(this->*m_Initialize)();
 UINT pos=0;
 for(; String[pos]; pos++)
 	{
@@ -98,6 +81,28 @@ for(; String[pos]; pos++)
 	(this->*m_AppendUnicode)(String[pos]);
 	}
 return pos;
+}
+
+VOID StringBuilder::Initialize(UINT len)
+{
+m_Buffer.clear();
+m_Position=0;
+if(len==0)
+	{
+	m_AppendAnsi=&StringBuilder::StringAppendAnsi;
+	m_AppendUnicode=&StringBuilder::StringAppendUnicode;
+	m_Size=0;
+	m_String=nullptr;
+	m_ToString=&StringBuilder::StringToString;
+	}
+else
+	{
+	m_AppendAnsi=&StringBuilder::StringAppendAnsi;
+	m_AppendUnicode=&StringBuilder::StringAppendUnicode;
+	m_Size=len+1;
+	m_String=String::Create(len, nullptr);
+	m_ToString=&StringBuilder::StringToString;
+	}
 }
 
 Handle<String> StringBuilder::ToString()
@@ -122,10 +127,6 @@ UINT StringBuilder::BufferAppendUnicode(WCHAR c)
 TCHAR tc=CharHelper::ToChar(c);
 m_Buffer.write(&tc, sizeof(TCHAR));
 return 1;
-}
-
-VOID StringBuilder::BufferInitialize()
-{
 }
 
 Handle<String> StringBuilder::BufferToString()
@@ -161,28 +162,15 @@ buf[m_Position++]=CharHelper::ToChar(c);
 return 1;
 }
 
-VOID StringBuilder::StringInitialize()
-{
-if(!m_String)
-	m_String=String::Create(m_Size-1, nullptr);
-}
-
 Handle<String> StringBuilder::StringToString()
 {
-if(!m_String)
-	return nullptr;
-if(m_Position+1<m_Size)
-	{
-	auto str=String::Create(m_Position, m_String->Begin());
-	m_Position=0;
-	return str;
-	}
+if(m_Position+1!=m_Size)
+	throw InvalidArgumentException();
 auto str=m_String;
 auto buf=const_cast<LPTSTR>(str->Begin());
 buf[m_Position]=0;
 str->m_Hash=StringHelper::GetHash(buf);
 str->m_Length=m_Position;
-m_String=nullptr;
-m_Position=0;
+Initialize(0);
 return str;
 }
--- StringBuilder.h
+++ StringBuilder.h
@@ -20,8 +20,7 @@ class StringBuilder
 {
 public:
 	// Con-/Destructors
-	StringBuilder();
-	StringBuilder(UINT Length);
+	StringBuilder(UINT Length=0);
 
 	// Common
 	UINT Append(CHAR Char);
@@ -30,27 +29,24 @@ public:
 	UINT Append(LPCWSTR String);
 	UINT Append(UINT Length, LPCSTR String);
 	UINT Append(UINT Length, LPCWSTR String);
+	VOID Initialize(UINT Length);
 	Handle<String> ToString();
 
 private:
 	// Functions
 	typedef UINT (StringBuilder::*APPEND_ANSI)(CHAR);
 	typedef UINT (StringBuilder::*APPEND_UNICODE)(WCHAR);
-	typedef VOID (StringBuilder::*INITIALIZE)();
 	typedef Handle<String> (StringBuilder::*TO_STRING)();
 
 	UINT BufferAppendAnsi(CHAR Char);
 	UINT BufferAppendUnicode(WCHAR Char);
-	VOID BufferInitialize();
 	Handle<String> BufferToString();
 	UINT StringAppendAnsi(CHAR Char);
 	UINT StringAppendUnicode(WCHAR Char);
-	VOID StringInitialize();
 	Handle<String> StringToString();
 	APPEND_ANSI m_AppendAnsi;
 	APPEND_UNICODE m_AppendUnicode;
 	Storage::packet_buf m_Buffer;
-	INITIALIZE m_Initialize;
 	UINT m_Position;
 	UINT m_Size;
 	Handle<String> m_String;
