41b430ba2cded6b46b362f1bb1a8467c9b88a46b
Sven Bieg <svenbieg@outlook.de>
2025-11-06 18:27:29 +0100

    128bit support

--- MemoryHelper.cpp
+++ MemoryHelper.cpp
@@ -30,30 +30,6 @@ for(SIZE_T u=0; u<size; u++)
 return 0;
 }
 
-INT MemoryHelper::Compare32(UINT const* buf1, UINT const* buf2, SIZE_T count)
-{
-for(SIZE_T u=0; u<count; u++)
-	{
-	if(buf1[u]>buf2[u])
-		return 1;
-	if(buf1[u]<buf2[u])
-		return -1;
-	}
-return 0;
-}
-
-INT MemoryHelper::Compare64(UINT64 const* buf1, UINT64 const* buf2, SIZE_T count)
-{
-for(SIZE_T u=0; u<count; u++)
-	{
-	if(buf1[u]>buf2[u])
-		return 1;
-	if(buf1[u]<buf2[u])
-		return -1;
-	}
-return 0;
-}
-
 VOID MemoryHelper::Copy(VOID* dst_ptr, VOID const* src_ptr, SIZE_T size)
 {
 auto dst=(LPSTR)dst_ptr;
@@ -63,58 +39,10 @@ while(dst<end)
 	*dst++=*src++;
 }
 
-VOID MemoryHelper::Copy32(UINT* dst, UINT const* src, SIZE_T count)
-{
-while(count--)
-	*dst++=*src++;
-}
-
-VOID MemoryHelper::Copy32(UINT* dst, UINT const* src, UINT* end)
-{
-while(dst<end)
-	*dst++=*src++;
-}
-
-VOID MemoryHelper::Copy64(UINT64* dst, UINT64 const* src, SIZE_T count)
-{
-while(count--)
-	*dst++=*src++;
-}
-
-VOID MemoryHelper::Copy64(UINT64* dst, UINT64 const* src, UINT64* end)
-{
-while(dst<end)
-	*dst++=*src++;
-}
-
 VOID MemoryHelper::Fill(VOID* dst_ptr, SIZE_T size, BYTE value)
 {
 auto dst=(BYTE*)dst_ptr;
 auto end=dst+size;
-while(dst<end)
-	*dst++=value;
-}
-
-VOID MemoryHelper::Fill32(UINT* dst, SIZE_T count, UINT value)
-{
-while(count--)
-	*dst++=value;
-}
-
-VOID MemoryHelper::Fill32(UINT* dst, UINT* end, UINT value)
-{
-while(dst<end)
-	*dst++=value;
-}
-
-VOID MemoryHelper::Fill64(UINT64* dst, SIZE_T count, UINT64 value)
-{
-while(count--)
-	*dst++=value;
-}
-
-VOID MemoryHelper::Fill64(UINT64* dst, UINT64* end, UINT64 value)
-{
 while(dst<end)
 	*dst++=value;
 }
@@ -135,46 +63,6 @@ if(dst>src)
 else
 	{
 	auto end=dst+size;
-	while(dst<end)
-		*dst++=*src++;
-	}
-}
-
-VOID MemoryHelper::Move32(UINT* dst_ptr, UINT const* src, SIZE_T count)
-{
-auto dst=dst_ptr;
-if(dst==src)
-	return;
-if(dst>src)
-	{
-	dst+=count;
-	src+=count;
-	while(dst>=dst_ptr)
-		*--dst=*--src;
-	}
-else
-	{
-	auto end=dst+count;
-	while(dst<end)
-		*dst++=*src++;
-	}
-}
-
-VOID MemoryHelper::Move64(UINT64* dst_ptr, UINT64 const* src, SIZE_T count)
-{
-auto dst=dst_ptr;
-if(dst==src)
-	return;
-if(dst>src)
-	{
-	dst+=count;
-	src+=count;
-	while(dst>=dst_ptr)
-		*--dst=*--src;
-	}
-else
-	{
-	auto end=dst+count;
 	while(dst<end)
 		*dst++=*src++;
 	}
--- MemoryHelper.h
+++ MemoryHelper.h
@@ -21,24 +21,61 @@ class MemoryHelper
 public:
 	// Common
 	static INT Compare(VOID const* Buffer1, VOID const* Buffer2, SIZE_T Size);
-	static INT Compare32(UINT const* Buffer1, UINT const* Buffer2, SIZE_T Count);
-	static INT Compare64(UINT64 const* Buffer1, UINT64 const* Buffer2, SIZE_T Count);
+	template <class _value_t> static INT CompareT(_value_t const* Values1, _value_t const* Values2, SIZE_T Count)
+		{
+		for(SIZE_T u=0; u<Count; u++)
+			{
+			if(Values1[u]>Values2[u])
+				return 1;
+			if(Values1[u]<Values2[u])
+				return -1;
+			}
+		return 0;
+		}
 	static VOID Copy(VOID* Destination, VOID const* Source, SIZE_T Size);
-	static VOID Copy32(UINT* Destination, UINT const* Source, SIZE_T Count);
-	static VOID Copy32(UINT* Destination, UINT const* Source, UINT* End);
-	static VOID Copy64(UINT64* Destination, UINT64 const* Source, SIZE_T Count);
-	static VOID Copy64(UINT64* Destination, UINT64 const* Source, UINT64* End);
+	template <class _value_t> static VOID CopyT(_value_t* Destination, _value_t const* Source, SIZE_T Count)
+		{
+		while(Count--)
+			*Destination++=*Source++;
+		}
+	template <class _value_t> static VOID CopyT(_value_t* Destination, _value_t const* Source, _value_t* End)
+		{
+		while(Destination<End)
+			*Destination++=*Source++;
+		}
 	static VOID Fill(VOID* Destination, SIZE_T Size, BYTE Value);
-	static VOID Fill32(UINT* Destination, SIZE_T Count, UINT Value);
-	static VOID Fill32(UINT* Destination, UINT* End, UINT Value);
-	static VOID Fill64(UINT64* Destination, SIZE_T Count, UINT64 Value);
-	static VOID Fill64(UINT64* Destination, UINT64* End, UINT64 Value);
+	template <class _value_t> static VOID FillT(_value_t* Destination, SIZE_T Count, _value_t Value)
+		{
+		while(Count--)
+			*Destination++=Value;
+		}
+	template <class _value_t> static VOID FillT(_value_t* Destination, _value_t* End, _value_t Value)
+		{
+		while(Destination<End)
+			*Destination++=Value;
+		}
 	static VOID Move(VOID* Destination, VOID const* Source, SIZE_T Size);
-	static VOID Move32(UINT* Destination, UINT const* Source, SIZE_T Count);
-	static VOID Move64(UINT64* Destination, UINT64 const* Source, SIZE_T Size);
+	template <class _value_t> static VOID MoveT(_value_t* Destination, _value_t const* Source, SIZE_T Count)
+		{
+		auto dst=Destination;
+		if(Destination==Source)
+			return;
+		if(Destination>Source)
+			{
+			auto end=Destination;
+			Destination+=Count;
+			Source+=Count;
+			while(Destination>=end)
+				*--Destination=*--Source;
+			}
+		else
+			{
+			auto end=Destination+Count;
+			while(Destination<end)
+				*Destination++=*Source++;
+			}
+		}
 	static inline VOID Zero(VOID* Destination, SIZE_T Size) { Fill(Destination, Size, 0); }
-	static inline VOID Zero32(UINT* Destination, SIZE_T Count) { Fill32(Destination, Count, 0); }
-	static inline VOID Zero32(UINT* Destination, UINT* End) { Fill32(Destination, End, 0); }
-	static inline VOID Zero64(UINT64* Destination, SIZE_T Count) { Fill64(Destination, Count, 0); }
-	static inline VOID Zero64(UINT64* Destination, UINT64* End) { Fill64(Destination, End, 0); }
+	template <class _value_t> static inline VOID ZeroT(_value_t* Destination, SIZE_T Count) { FillT<_value_t>(Destination, Count, _value_t(0)); }
+	template <class _value_t> static inline VOID ZeroT(_value_t* Destination, _value_t* End) { FillT<_value_t>(Destination, End, _value_t(0)); }
 };
