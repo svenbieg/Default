52d7e29173a999a38e156951300724962f006b63
Sven Bieg <svenbieg@outlook.de>
2026-01-04 14:51:31 +0100

    static_assert

--- FlagHelper.h
+++ FlagHelper.h
@@ -5,13 +5,6 @@
 #pragma once
 
 
-//=======
-// Using
-//=======
-
-#include "TypeHelper.h"
-
-
 //========
 // Common
 //========
@@ -19,30 +12,37 @@
 class FlagHelper
 {
 public:
+	// Using
+	using uint32_t=unsigned int;
+
 	// Common
-	template <class _flags_t, class _flag_t>
-	static inline VOID Clear(_flags_t& Flags, _flag_t Flag)noexcept
+	template <class _flags_t, class _flag_t> static inline void Clear(_flags_t& Flags, _flag_t Flag)noexcept
 		{
-		UINT& flags=(UINT&)Flags;
-		UINT flag=(UINT)Flag;
+		static_assert(sizeof(_flags_t)<=4);
+		static_assert(sizeof(_flag_t)<=4);
+		auto flags=static_cast<uint32_t>(Flags);
+		auto flag=static_cast<uint32_t>(Flag);
 		flags&=~flag;
+		Flags=(_flags_t)flags;
 		}
-	template <class _flags_t, class _flag_t>
-	static inline BOOL Get(_flags_t Flags, _flag_t Flag)noexcept
+	template <class _flags_t, class _flag_t> static inline bool Get(_flags_t Flags, _flag_t Flag)noexcept
 		{
-		UINT flags=(UINT)Flags;
-		UINT flag=(UINT)Flag;
+		static_assert(sizeof(_flags_t)<=4);
+		static_assert(sizeof(_flag_t)<=4);
+		auto flags=static_cast<uint32_t>(Flags);
+		auto flag=static_cast<uint32_t>(Flag);
 		return (flags&flag)>0;
 		}
-	template <class _flags_t, class _flag_t>
-	static inline VOID Set(_flags_t& Flags, _flag_t Flag)noexcept
+	template <class _flags_t, class _flag_t> static inline void Set(_flags_t& Flags, _flag_t Flag)noexcept
 		{
-		UINT& flags=(UINT&)Flags;
-		UINT flag=(UINT)Flag;
+		static_assert(sizeof(_flags_t)<=4);
+		static_assert(sizeof(_flag_t)<=4);
+		auto flags=static_cast<uint32_t>(Flags);
+		auto flag=static_cast<uint32_t>(Flag);
 		flags|=flag;
+		Flags=(_flags_t)flags;
 		}
-	template <class _flags_t, class _flag_t>
-	static inline VOID Set(_flags_t& Flags, _flag_t Flag, BOOL Set)noexcept
+	template <class _flags_t, class _flag_t> static inline void Set(_flags_t& Flags, _flag_t Flag, bool Set)noexcept
 		{
 		Set? FlagHelper::Set(Flags, Flag): FlagHelper::Clear(Flags, Flag);
 		}
