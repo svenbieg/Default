c5abfc543023574a6e4f1608178b5a034e1bcb42
Sven Bieg <svenbieg@outlook.de>
2026-04-23 15:17:57 +0200

    Updated Function

--- Function.h
+++ Function.h
@@ -26,7 +26,7 @@ public:
 	Function(Function&& Move)noexcept: m_Callable(Move.m_Callable) { Move.m_Callable=nullptr; }
 	template<class _owner_t, class _lambda_t> Function(_owner_t* Owner, _lambda_t&& Lambda)
 		{
-		m_Callable=CallableTyped<_owner_t, _lambda_t, _ret_t, _args_t...>::Create(Owner, std::forward<_lambda_t>(Lambda));
+		m_Callable=CallableLambda<_owner_t, _lambda_t, _ret_t, _args_t...>::Create(Owner, std::forward<_lambda_t>(Lambda));
 		}
 
 	// Common
