d2a0b9b5d6e79ce0f60c2b3746062052097811b0
Sven Bieg <svenbieg@outlook.de>
2026-06-04 18:13:13 +0200

    Typing error

--- Global.h
+++ Global.h
@@ -2,7 +2,7 @@
 // Global.h
 //==========
 
-// Using objectes randomly in multiple tasks.
+// Using unique objects randomly in multiple tasks.
 
 // Copyright 2026, Sven Bieg (svenbieg@outlook.de)
 // https://github.com/svenbieg/Default
